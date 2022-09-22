b8ca2f26806626045636f3ba8d872a410c550aac
yAntPower <1561716982@qq.com>
2022-09-22 09:53:38 +0800

    优化了一些代码

--- readfile/src/main.rs
+++ readfile/src/main.rs
@@ -7,19 +7,19 @@ fn readfile(dir: &Path) -> io::Result<()> {
         for result_dir in fs::read_dir(dir)? {
             let direntry = result_dir?;
             let path = direntry.path();
+            println!("当前路径：{}",path.to_str().unwrap());
             if path.is_dir() {
+                println!("发现文件夹开始递归！");
                 readfile(&path)?;
             } else {
-                if let Some(name) = path.file_name() {
-                    if let Some(filename) = name.to_str() {
-                        match fs::read_to_string(&path) {
-                            Ok(context) => {
-                                println!("文件{}的内容为{}", filename, context);
-                            }
-                            Err(_) => {
-                                continue;
-                            }
-                        }
+                let filename = path.file_name().unwrap().to_str().unwrap();
+                match fs::read_to_string(&path) {
+                    Ok(context) => {
+                        println!("文件{}的内容为{}", filename, context);
+                    }
+                    Err(e) => {
+                        println!("路径：{},错误:{}",path.to_str().unwrap(),e);
+                        continue;
                     }
                 }
             }
